4580356412a9a210d13d355b7def722daa5a2512
Derick Muluka <mulukaderick@gmail.com>
2024-06-24 09:14:12 +0300

    add Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+import Camp from "@/components/Camp";
+import Featues from "@/components/Featues";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <h1 className="font-bold text-teal-500 text-2xl">Hello</h1>
-  );
+    <>
+    <Hero />
+    <Camp />
+    <Guide />
+    <Featues />
+    <GetApp />
+    </>
+  )
 }
